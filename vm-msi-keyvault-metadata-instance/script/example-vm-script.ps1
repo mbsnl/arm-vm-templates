dd391ef3dc1f48c44660a621d65471ad45c1533c
Menno Stevens (SP3) <no-spam@xs4all.nl>
2018-06-04 19:49:34 +0200

    Update/Initial

--- vm-msi-keyvault-metadata-instance/script/example-vm-script.ps1
+++ vm-msi-keyvault-metadata-instance/script/example-vm-script.ps1
@@ -1,5 +1,5 @@
 ﻿[CmdletBinding()]
-param(    
+param(
 )
 
 # Source of parameters is metadata-instance: Azure VM Tags according to our tag definition
@@ -29,22 +29,22 @@ try {
 
     $access_token=Invoke-RestMethod -Method Get -Uri 'http://169.254.169.254/metadata/identity/oauth2/token?api-version=2018-02-01&resource=https%3A%2F%2Fvault.azure.net' -Headers @{Metadata="true"}|Select-Object -ExpandProperty access_token
 }
-catch {        
+catch {
     Write-Error "Error (20): $($Error[0]) - $($Error[0].Exception.InnerException)" -ErrorAction Stop
 }
 
-# Update KeyVault with new Secret 
+# Update KeyVault with new Secret
 #
 # Docs:
 # https://docs.microsoft.com/en-us/rest/api/keyvault/setsecret/setsecret
 
 try {
-    # Update KeyVault with new Secret/Password   
-    $SecretVault='value that is stored in KeyVault' 
+    # Update KeyVault with new Secret/Password
+    $SecretVault='value that is stored in KeyVault'
     $Uri='https://{0}.vault.azure.net/secrets/{1}?api-version=2016-10-01' -f $keyvaultName,'secretname-from-script'
     $restResult=Invoke-RestMethod -Method Put -Uri $Uri -Headers @{Authorization="Bearer $access_token"} -ContentType 'application/json' -Body (@{value=$SecretVault}|ConvertTo-Json)
 }
-catch {        
+catch {
     Write-Error "Error (30): $($Error[0]) - $($Error[0].Exception.InnerException)" -ErrorAction Stop
 }
 
